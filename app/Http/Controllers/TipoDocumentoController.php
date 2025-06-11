f5239727c069c373c5c44549bf26c7c1a25e600c
camila granada <Maria.granada@correounivalle.edu.co>
2025-06-11 09:39:04 -0500

    creacion de funcion modificar tipo documento

--- app/Http/Controllers/TipoDocumentoController.php
+++ app/Http/Controllers/TipoDocumentoController.php
@@ -19,4 +19,46 @@ class TipoDocumentoController extends Controller
         ]);
         return redirect()->back()->with('success', 'tipo de documento guardado con exito');
     }
+
+    public function eliminar(Request $request)
+    {
+        $request -> validate ([
+            'id' => 'required|numeric'
+        ]);
+
+        $tipoDocumento = TipoDocumento::find($request->id);
+
+        if($tipoDocumento){
+            $tipoDocumento -> delete();
+
+            return redirect()->back()->with('success', 'el tipo de documento se elimino correctamente');
+        
+        }else{
+            return redirect()->back()->with('error', 'no se encontro el id de ese tipo de documento');
+        }
+
+    }
+
+    public function modificar (Request $request)
+    {
+        $request -> validate ([
+            'id' => 'required|numeric',
+            'nombre' => 'required|string|max:100',
+            'descripcion' => 'required|string|max:250'
+        ]);
+
+        $tipoDocumento = TipoDocumento::find($request->id);
+
+        if($tipoDocumento){
+            $tipoDocumento -> update([
+                'nombreTipoDoc' => $request->nombre,
+                'descripcionTipoDoc' => $request->descripcion
+            ]);
+
+            return redirect()->back()->with('success', 'tipo de documento modificado correctamente');
+
+        }else {
+            return redirect()->back()->with('error', 'no se encontro el id de tipo documento');
+        }
+    }
 }
